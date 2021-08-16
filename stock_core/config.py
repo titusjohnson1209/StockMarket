3849637a0bab71a5f66ee92a5f957ba7e6fabb02
titusjohnson1209 <88936842+titusjohnson1209@users.noreply.github.com>
2021-08-16 12:30:44 -0700

    Add files via upload

---
+++ stock_core/config.py
@@ -0,0 +1,19 @@
+elasticsearch_host = "localhost"
+elasticsearch_port = 9200
+elasticsearch_user = "stockproject19@gmail.com"
+elasticsearch_password = "stock@123"
+consumer_key = "gO9LOC5dv8Ct1ZTb9uX56Jm6a"
+consumer_secret = "byfWkEn7htlZKjzHUwIwoD4oGrSSJuwTuOiTo6bcIiiXxkUdvc"
+access_token = "1115239548399656961-wZI1YrJLbACFJDVf9vwZqz4U3V6VXY"
+access_token_secret = "I6OPoJPya1yAITi4N0TeTvr18Ta3TcFRNDcZ7ZFXSaMf1"
+nltk_tokens_required = ("Tesla", "@Tesla", "#Tesla", "tesla", "TSLA", "tsla", "#TSLA", "#tsla", "elonmusk", "Elon", "Musk")
+nltk_tokens_ignored = ("win", "Win", "giveaway", "Giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
